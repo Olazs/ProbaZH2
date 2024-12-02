9a20642009642748f8722dfa2cf9e822ca5a3d79
Olazs <oliverbalazsgal@outlook.com>
2024-12-03 00:30:55 +0100

    Negyedik feladat update

--- 4.c
+++ 4.c
@@ -1,3 +1,35 @@
-//
-// Created by Olazs on 2024. 12. 03..
-//
+#include <stdio.h>
+
+int program4() {
+    int num1, num2;
+    float average;
+    FILE *file;
+
+    // Két szám bekérése
+    printf("Kérlek, add meg az első számot: \n");
+    scanf("%d", &num1);
+    printf("Kérlek, add meg a második számot: \n");
+    scanf("%d", &num2);
+
+    // A számtani közép kiszámítása
+    average = (num1 + num2) / 2.0;  // Az osztás eredménye lebegőpontos szám lesz
+
+    // Fájl megnyitása írásra, ha nem létezik, akkor létrehozza
+    file = fopen("adat.txt", "w");
+
+    // Ha a fájl sikeresen megnyílt
+    if (file != NULL) {
+        // A számtani közép kiírása a fájlba
+        fprintf(file, "A két szám számtani közepe: %.2f\n", average);
+
+        // A fájl bezárása
+        fclose(file);
+
+        printf("A számtani közép sikeresen kiírva az adat.txt fájlba.\n");
+    } else {
+        // Ha nem sikerül megnyitni a fájlt
+        printf("A fájl megnyitása sikertelen.\n");
+    }
+
+    return 0;
+}
